e2c8763d95e5a27b6535236e18d3d1fbe6c44240
Christophe Meessen <christophe@meessen.net>
2019-03-04 10:16:13 +0100

    cached connection and synchronous transactions

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ import (
 
 var timeOutDelay = 30 * time.Second
 
-// check that the server’s name in the certificate matches the hosst name
+// check that the server’s name in the certificate matches the host name
 const serverDNSNameCheck = false
 
 func runAsClient() {
@@ -29,48 +29,57 @@ func runAsClient() {
 		Message:   "no problem",
 	}
 	statStart(time.Duration(*periodFlag) * time.Second)
+	lms := &MsgLogSrv{Address: *addressFlag}
 	for {
 		m.Stamp = time.Now().UTC()
-		n, err := sendMessage(&m)
-		if err != nil {
-			log.Fatalf("send message: %+v", err)
+		n := lms.SendMessage(&m)
+		if lms.err != nil {
+			log.Printf("send message: %+v, wait 2 seconds", lms.err)
+			time.Sleep(2 * time.Second)
 		}
 		statUpdate(n)
 	}
 }
 
-func sendMessage(m *dmon.Msg) (int, error) {
-	var (
-		err  error
-		conn net.Conn
-	)
-	// open connection
-	if *tlsFlag {
-		var clientCert tls.Certificate
-		clientCert, err = tls.LoadX509KeyPair(clientCRTFilename, clientKeyFilename)
-		if err != nil {
-			return 0, errors.Wrapf(err, "could not load X509 certificate")
+// MsgLogSrv holds a cached connection to the logging server.
+type MsgLogSrv struct {
+	Address string
+	conn    net.Conn
+	err     error
+}
+
+// Error returns the last error.
+func (lms *MsgLogSrv) Error() error {
+	return lms.err
+}
+
+// SendMessage send the message m to the logging server.
+func (lms *MsgLogSrv) SendMessage(m *dmon.Msg) (n int) {
+	defer func() {
+		if lms.conn != nil && lms.err != nil {
+			lms.conn.Close()
+			lms.conn = nil
 		}
-		config := tls.Config{
-			Certificates:       []tls.Certificate{clientCert},
-			InsecureSkipVerify: !serverDNSNameCheck,
-		}
-		conn, err = tls.Dial("tcp", *addressFlag, &config)
-	} else {
-		conn, err = net.Dial("tcp", *addressFlag)
+	}()
+
+	if lms.conn == nil || lms.err != nil {
+		lms.tryConnect()
 	}
-	if err != nil {
-		return 0, errors.Wrap(err, "open connection failed")
+	if lms.conn == nil || lms.err != nil {
+		return 0
 	}
-	defer conn.Close()
 
 	// encode message
 	buf := make([]byte, 8, 512)
 	switch *msgCodecFlag {
 	case "json":
-		buf, err = m.JSONEncode(buf)
+		buf, lms.err = m.JSONEncode(buf)
 	case "binary":
-		buf, err = m.BinaryEncode(buf)
+		buf, lms.err = m.BinaryEncode(buf)
+	}
+	if lms.err != nil {
+		lms.err = errors.Wrap(lms.err, "send message")
+		return 0
 	}
 
 	// set message header
@@ -78,30 +87,64 @@ func sendMessage(m *dmon.Msg) (int, error) {
 	binary.LittleEndian.PutUint32(buf[4:8], uint32(len(buf)-8))
 
 	// send message
-	conn.SetWriteDeadline(time.Now().Add(timeOutDelay))
-	n, err := conn.Write(buf)
-	if err != nil {
-		return 0, errors.Wrap(err, "send message")
+	lms.err = lms.conn.SetWriteDeadline(time.Now().Add(timeOutDelay))
+	if lms.err != nil {
+		lms.err = errors.Wrap(lms.err, "send message")
+		return 0
+	}
+
+	n, lms.err = lms.conn.Write(buf)
+	if lms.err != nil {
+		lms.err = errors.Wrap(lms.err, "send message")
+		return 0
 	}
 	if n != len(buf) {
-		err = errors.Errorf("short write: expected %d, got %d", len(buf), n)
-		return 0, errors.Wrap(err, "send message")
+		lms.err = errors.Errorf("short write: expected %d, got %d", len(buf), n)
+		lms.err = errors.Wrap(lms.err, "send message")
+		return 0
 	}
 
 	// receive acknowledgment
 	var b [1]byte
-	conn.SetReadDeadline(time.Now().Add(timeOutDelay))
-	n, err = conn.Read(b[:])
+	lms.err = lms.conn.SetReadDeadline(time.Now().Add(timeOutDelay))
+	if lms.err != nil {
+		lms.err = errors.Wrap(lms.err, "recv acknowledgment")
+		return 0
+	}
+	n, lms.err = lms.conn.Read(b[:])
 	if n != 1 {
-		if err != nil {
-			return 0, errors.Wrap(err, "recv acknowledgment")
+		if lms.err != nil {
+			lms.err = errors.Wrap(lms.err, "recv acknowledgment")
+			return 0
 		}
-		err = errors.Errorf("expected 1 byte, got %d", n)
-		return 0, errors.Wrap(err, "recv acknowledgment")
+		lms.err = errors.Errorf("expected 1 byte, got %d", n)
+		lms.err = errors.Wrap(lms.err, "recv acknowledgment")
+		return 0
 	}
 	if b[0] != ackCode {
-		err = errors.Errorf("expected ack byte %+X, got %+X", ackCode, b[0])
-		return 0, errors.Wrap(err, "recv acknowledgment")
+		lms.err = errors.Errorf("expected ack byte %+X, got %+X", ackCode, b[0])
+		lms.err = errors.Wrap(lms.err, "recv acknowledgment")
+		return 0
 	}
-	return len(buf), nil
+	return len(buf)
+}
+
+func (lms *MsgLogSrv) tryConnect() {
+	var err error
+	if *tlsFlag {
+		var clientCert tls.Certificate
+		clientCert, err = tls.LoadX509KeyPair(clientCRTFilename, clientKeyFilename)
+		if err != nil {
+			lms.conn = nil
+			lms.err = errors.Wrapf(err, "could not load X509 certificate")
+			return
+		}
+		config := tls.Config{
+			Certificates:       []tls.Certificate{clientCert},
+			InsecureSkipVerify: !serverDNSNameCheck,
+		}
+		lms.conn, lms.err = tls.Dial("tcp", *addressFlag, &config)
+	} else {
+		lms.conn, lms.err = net.Dial("tcp", *addressFlag)
+	}
 }
